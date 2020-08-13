6b7777b4230c4af6a96eeb384b2a98d337cf46dc
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 01:58:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,13 +5,6 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  #array.count { |i| i == ""} 
-  i = 0
-  counter = 0
-  while i<array.count do
-    if array[i] == ""
-      counter += 1
-    end
-    i+=1
-    return counter
+  array.count { |i| i == ""} 
+
 end
